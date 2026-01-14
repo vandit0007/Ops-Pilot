25eeac48871d0208dc60012c5333e677ff32728d
vandit0007 <vanditpandey2828@gmail.com>
2026-01-14 15:54:05 +0530

    Add files via upload

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,10 @@
+module.exports = (requiredRole) => {
+  return (req, res, next) => {
+    if (req.user.role !== requiredRole) {
+      return res
+        .status(403)
+        .json({ msg: "Access denied: insufficient permissions" });
+    }
+    next();
+  };
+};
